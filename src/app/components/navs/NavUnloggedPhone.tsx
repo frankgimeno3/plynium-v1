68ee5be32bfc9cf8028b163bdff32825874df611
frank gimeno <frankgimeno3@gmail.com>
2024-09-27 18:26:21 +0200

    eslint modificado

---
+++ src/app/components/navs/NavUnloggedPhone.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface NavUnlogggedPhoneProps {
+  
+}
+
+const NavUnlogggedPhone: FC<NavUnlogggedPhoneProps> = ({ }) => {
+  return (
+    <div>NavUnlogggedPhone</div>
+  );
+};
+
+export default NavUnlogggedPhone;
---
+++ src/app/components/navs/NavUnloggedWeb.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface NavUnlogggedWebProps {
+  
+}
+
+const NavUnlogggedWeb: FC<NavUnlogggedWebProps> = ({ }) => {
+  return (
+    <div>NavUnlogggedWeb</div>
+  );
+};
+
+export default NavUnlogggedWeb;
